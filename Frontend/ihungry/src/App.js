c3391db51cb41453dca5759cb8f662b48f5637ea
Liumyss <92648495+Liumyss@users.noreply.github.com>
2022-06-23 21:28:28 -0600

    react ready

---
+++ Frontend/ihungry/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+
+      <h1>Ne me touche pas</h1>
+    </div>
+  );
+}
+
+export default App;
