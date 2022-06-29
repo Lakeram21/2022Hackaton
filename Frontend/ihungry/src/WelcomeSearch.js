b469f3000bfa1a0205f8ef31b430a0db77cd21fa
ronald <ronaldcoello85@gmail.com>
2022-06-28 23:54:39 -0600

    restaurant

---
+++ Frontend/ihungry/src/WelcomeSearch.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './WelcomeSearch.css'
+import {FaSearch} from 'react-icons/fa'
+import { data } from 'autoprefixer'
+
+function WelcomeSearch(props) {
+    return (
+        <div className='welcome-search'>
+                <h1>{props.dataRest.name}</h1>
+                <i style={{position:'absolute', marginTop:'35px', marginLeft:'250px', color:'#9ca3af'}}>{<FaSearch size={'20px'}/>}</i>
+                <input className='search-bar' type={'text'} placeholder={'Search'}>
+                    
+                </input>
+        </div>
+    )
+}
+
+export default WelcomeSearch
