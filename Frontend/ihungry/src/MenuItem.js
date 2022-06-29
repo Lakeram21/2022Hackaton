01db0baaccec610e26f43bced3ffa93491a144a7
Lakeram Narine <nar19002@byui.edu>
2022-06-29 10:41:48 -0600

    fixed the map in menuItem

--- Frontend/ihungry/src/MenuItem.js
+++ Frontend/ihungry/src/MenuItem.js
@@ -5,6 +5,7 @@ import spice from './img/spicy-sandwich.png'
 
 
 function MenuItem({menu}) {
+    const menu2= menu
 console.log("This is menu", menu);
     return (
         <div className='menu-item'>
@@ -16,12 +17,12 @@ console.log("This is menu", menu);
             <span className='price'>$4.90</span>
             <button >Add</button> */}
 
-            {Object.entries(menu).map((key, value) => (
+            {Object.keys(menu2).map((key, value) => (
                 <div className='ItemContainer'>
                     <div className='img'><img src={spice} alt="item-image"></img></div>
                     <h1 className='title'>{key}</h1>
                     <p className='description'>A boneless breast of chicken seasoned to perfection, freshly breaded,</p>
-                    <p className='price'>{value}</p>
+                    <p className='price'>${menu2[key]}</p>
                     <button >Add</button> 
                 </div>
             ))}
--- Frontend/ihungry/src/RestaurantPage.js
+++ Frontend/ihungry/src/RestaurantPage.js
@@ -1,13 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
-<<<<<<< HEAD
-import MenuItem from './MenuItem';
-
-=======
 import WelcomeSearch from './WelcomeSearch';
 import './RestaurantPage.css'
 import MenuItem from './MenuItem';
->>>>>>> 2810b5c36a527cc624c4896d385abd2717ad96b1
 function RestaurantPage({restaurant}) {
     const [selectedResturant, setSelectedRest] = useState()
     const location = useLocation();
