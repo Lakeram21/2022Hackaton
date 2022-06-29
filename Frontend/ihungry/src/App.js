ee9ddc4f69f0d65d41ecafa251e6cea3a347ef21
Lesliee Cruz <lesliee.icc@gmail.com>
2022-06-28 22:57:59 -0600

    Lesliee stuff

--- Frontend/ihungry/src/App.js
+++ Frontend/ihungry/src/App.js
@@ -5,6 +5,7 @@ import SideBar from "./SideBar";
 import "./RightBar";
 import RightBar from "./RightBar";
 import Order from "./controller/Orders"
+import MenuItem from "./MenuItem";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -16,9 +17,11 @@ function App() {
       <Router>
       <SideBar />
       <RightBar />
+      <MenuItem/>
       <Routes>
         <Route path ='/' exact element= {<MainMenu/>}/>
         <Route path='/orders' exact element={<Order />} />
+
       </Routes>
     </Router>
     </div>
