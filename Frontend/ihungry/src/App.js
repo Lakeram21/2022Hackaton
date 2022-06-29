e50ba3b2adfe470e85650e74075d8958c54327ee
Liumyss <92648495+Liumyss@users.noreply.github.com>
2022-06-29 00:03:00 -0600

    More changes in RightBar

--- Frontend/ihungry/src/App.js
+++ Frontend/ihungry/src/App.js
@@ -1,26 +1,23 @@
 import "./App.css";
 import MainMenu from "./MainMenu";
-import "./SideBar";
 import SideBar from "./SideBar";
-import "./RightBar";
 import RightBar from "./RightBar";
-import Order from "./controller/Orders"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Checkout from "./controller/Checkout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-     
 
       <Router>
-      <SideBar />
-      <RightBar />
-      <Routes>
-        <Route path ='/' exact element= {<MainMenu/>}/>
-        <Route path='/orders' exact element={<Order />} />
-      </Routes>
-    </Router>
+        <SideBar />
+        <RightBar />
+        <Routes>
+          <Route path="/" exact element={<MainMenu />} />
+          <Route path="/Checkout" exact element={<Checkout />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
